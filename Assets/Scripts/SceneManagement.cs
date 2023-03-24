bbf7c522ac10b9b9db8451297a4fe41d4d9f6ad4
emilyju0314 <103551088+emilyju0314@users.noreply.github.com>
2023-03-24 12:06:24 +0800

    死亡畫面

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,30 +7,39 @@ using UnityEngine.SceneManagement;
 public class SceneManagement : MonoBehaviour
 {
     private int playerCount;
-
+    public GameObject canvas_;
+    public changeText text_;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
+        text_ = new changeText();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+            changeScene();
         playerCount = GameObject.FindGameObjectsWithTag("Player").Length;
+        Debug.Log(Time.timeScale);
         if(playerCount == 1)
         {
             Time.timeScale = 0;
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-                Time.timeScale = 1;
-            }
+            canvas_.SetActive(true);
+            //GameObject.Find("changeText").SendMessage("changetext");
+            //text_.changetext(GameObject.FindGameObjectsWithTag("Player").ToString());
         }
     }
 
     internal static void LoadScene(string v)
     {
         throw new NotImplementedException();
+    }
+    public void changeScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        canvas_.SetActive(false);
     }
 }
